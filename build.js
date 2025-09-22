ef6208f3931e9f5523fb7bf01966f240a8d82e25
Philip Jägenstedt <philip@foolip.org>
2025-09-22 15:15:12 +0200

    Update throttling config

--- build.js
+++ build.js
@@ -40,16 +40,21 @@ async function main() {
     auth: process.env.GITHUB_TOKEN,
     throttle: {
       onRateLimit: (retryAfter, options) => {
-        console.log('');
+        octokit.log.warn(
+          `Request quota exhausted for request ${options.method} ${options.url}`,
+        );
+  
+        // Retry twice after hitting a rate limit error, then give up
         if (options.request.retryCount <= 2) {
-          console.warn(`Rate limiting triggered, retrying after ${retryAfter} seconds!`);
+          console.log(`Retrying after ${retryAfter} seconds!`);
           return true;
-        } else {
-          console.error(`Rate limiting triggered, not retrying again!`);
         }
       },
-      onAbuseLimit: () => {
-        console.error('Abuse limit triggered, not retrying!');
+      onSecondaryRateLimit: (retryAfter, options, octokit) => {
+        // does not retry, only logs a warning
+        octokit.log.warn(
+          `Secondary quota detected for request ${options.method} ${options.url}`,
+        );
       },
     },
   });
